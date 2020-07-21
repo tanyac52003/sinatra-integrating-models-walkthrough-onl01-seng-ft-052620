bbe66c62037dae8e09d8b99c3d09ed048f7c41ba
Tanya Hodges <tanyac52003@gmail.com>
2020-07-21 20:59:30 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,20 @@
 # Your TextAnalyzer model code will go here.
+class TextAnalyzer
+  attr_reader :text
+ 
+  def initialize(text)
+    @text = text.downcase
+  end
+ 
+  def count_of_words
+    words = text.split(" ")
+    words.count
+  end
+ 
+  def count_of_vowels
+    text.scan(/[aeoui]/).count
+  end
+ 
+  def count_of_consonants
+    text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+  end
